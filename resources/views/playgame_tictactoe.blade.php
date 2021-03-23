762655b515e78196bbb5c3fd73c7c1acb30e4651
khunbank <khunbankkum@gmail.com>
2021-03-23 10:43:16 +0700

    resources/views/playgame_tictactoe.blade:

--- resources/views/playgame_tictactoe.blade.php
+++ resources/views/playgame_tictactoe.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
           <!-- Favicon-->
-          <link rel="icon" type="image/x-icon" href="{{ asset('imgs/profile.svg') }}" sizes="16x16"/>
+          <link rel="icon" type="image/x-icon" href="{{ asset('/imgs/profile.svg') }}" sizes="16x16"/>
     <title>Game Tic tac toe</title>
     <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
