26c787cd4ce7a372b70a6a4bc3056f46a8ce1a0e
khunbank <khunbankkum@gmail.com>
2021-03-25 10:20:12 +0700

    add modal

--- resources/views/playgame_tictactoe.blade.php
+++ resources/views/playgame_tictactoe.blade.php
@@ -85,12 +85,33 @@
 
     </div>
 
+    <!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">แจ้งเตือน</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">ยกเลิก</button>
+          <button type="button" class="btn btn-primary">ตกลง</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
 
     
    <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 <script
-  src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script><script>
+  src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <script>
+      var myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
     var arrBorad     = [];
     var gamePlay     = "play1";
     var namePlayer1  = "";
@@ -223,11 +244,13 @@
             namePlayer = namePlayer2;
         }
 
-        if(confirm(namePlayer+" YouWin. Reset plase ok.")){
-            namePlayWin = namePlayer;
-            ApiCreate();
-            Reload();
-        }
+        myModal..show();
+
+        // if(confirm(namePlayer+" YouWin. Reset plase ok.")){
+        //     namePlayWin = namePlayer;
+        //     ApiCreate();
+        //     Reload();
+        // }
      
     }
 
